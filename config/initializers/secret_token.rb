1a8bb44269410a96ccdf5d7e7e171e1a4a7bc8af
GonzoGhanem <ghanemgo@gmail.com>
2014-12-22 14:04:00 -0300

    Initial commit (Nene Demo)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nenep::Application.config.secret_key_base = 'c2a27f976807be929dd78c198f6513dd9dc77645f46aacc2bd9867283abc95f74eca9c4d0c673f474871c6716cea90d4bcb9fdc4a3fe80c2907abe0883f74d48'
