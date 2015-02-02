5cd21446993922ebf76e5e097fbf4df84f283787
Guido Alloatti <galloatti-c@g2llc.com>
2015-02-02 18:37:46 -0300

    New style

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "Nene", email: "nene@nenep.com.ar", password: "admin123")
-User.create(name: "Gonzo", email: "gonzo@gonzoft.com.ar", password: "admin123")
+User.create(name: "Guido", fullname: "Guido Alloatti", email: "guido.alloatti@gonzoft.com.ar", password: "admin123")
+User.create(name: "Nene", fullname: "Gonzalo Ghanem", email: "nene@nenep.com.ar", password: "admin123")
+User.create(name: "Gonzo", fullname: "Nené Peñaranda", email: "gonzo@gonzoft.com.ar", password: "admin123")
 
 # Article.delete_all
 # Article.create(title: "Titulo", author: "Gonzo", date: Time.now,detail: "Titulo mas chico, algun dato", text: "Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.", visible: true)
